259f3281ea62fe1f929bf5175368678234f7cdf7
darkwolf3411 <kirill.vernikovskiy@mail.ru>
2020-12-10 20:19:02 +0300

    init

--- src/component/Content/Service/Item/Item.js
+++ src/component/Content/Service/Item/Item.js
@@ -8,9 +8,11 @@ const Item = (props) => {
             <div className={Style.item}>
                 <img src={props.src}></img>
                 <div>
-                    <NavLink to={props.nav}>
-                        {props.header}
-                    </NavLink>
+                    <div>
+                        <NavLink to={props.nav}>
+                            {props.header}
+                        </NavLink>
+                    </div>
                     <span>
                         {props.content}
                     </span>
@@ -30,27 +32,32 @@ const Items = (props) => {
             src: "https://33коттеджа.рф/upload/iblock/5bc/5bc10d13be910f08927d34d3d5baddf3.PNG",
             header: "Выбор земельного участка под строительство",
             content: "Выбор земельного участка — одна из ключевых задач при строительстве жилого объекта.",
-            nav : "1"
+            nav: "/service/1"
         },
         {
             src: "https://33коттеджа.рф/upload/iblock/32e/32e8eab30b1f1293ff3e9e8265378ab5.PNG",
             header: "Строительство домов и коттеджей",
             content: "Строительство дома - самый трудоемкий, длительный и важный этап в полном цикле возведения объекта недвижимости..",
-            nav : "2"
+            nav: "/service/2"
         },
         {
             src: "https://33коттеджа.рф/upload/iblock/a7b/a7b191beda6653ab4e1445248832fd06.PNG",
             header: "Дизайн",
             content: "Вы мечтаете о собственном доме. Вы представляете его себе красивым, удобным и уютным.  И кажется, это так просто....",
-            nav : "3"
+            nav: "/service/3"
         },
         {
             src: "https://33коттеджа.рф/upload/iblock/a99/a99ed3f8e63acb88468fccd07d772abb.PNG",
             header: "Строительство бань, беседок и других построек",
             content: "Наличие свободного участка земли подразумевает строительство дома или коттеджа. Если сам дом уже построен, можно позаботиться о строительстве бани.",
-            nav : "4"
+            nav: "/service/4"
         },
-
+        {
+            src: "https://33коттеджа.рф/upload/iblock/77d/77d8f827eb5599c99d82d7e6291bccc1.PNG",
+            header: "Благоустройство территории",
+            content: "Красивый респектабельный облик дому помогает придать гармоничное благоустройство территории.",
+            nav: "/service/5"
+        },
     ];
 
     let itemElement = urls.map(el => <Item src={el.src} header={el.header} content={el.content} nav={el.nav} />)
